ac8fdf6d53701cf525387ac13dec491298b89947
Tatiana Haveroth Barbosa <haverothtatiana@gmail.com>
2024-11-04 11:55:02 -0300

    atualizando a foto

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import Container from "./Container";
 import styles from './Navbar.module.css';
-
+import fotoAlisson from './../../img/fotoalisson.jpg'; // ajuste a extensão conforme necessário
 
 function Navbar() {
     return (
@@ -9,6 +10,7 @@ function Navbar() {
             <Container>
                 <div className={styles.profile}>
                     <Link to="/">
+                        <img src={fotoAlisson} alt="Prof. Dr. Alisson Barbosa de Sousa" className={styles.foto} />
                     </Link>
                     <div className={styles.info}>
                         <h1 className={styles.title}>Prof. Dr. Alisson Barbosa de Sousa</h1>
@@ -22,7 +24,6 @@ function Navbar() {
                             Currículo Lattes
                         </a>
                         <p className={styles.emailLink}>E-mail: <a href="mailto:alisson@ufc.br">alisson@ufc.br</a></p>
-                    
                     </div>
                 </div>
                 <ul className={styles.list}>
